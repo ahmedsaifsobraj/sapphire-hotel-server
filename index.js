6c9951fe1d4902d61b473dab5bcb8612c0a66f8e
ahmedsaifsobraj <ahmedsaifsobraj@gmail.com>
2024-11-07 02:49:34 +0600

    last

--- index.js
+++ index.js
@@ -2,51 +2,139 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 require("dotenv").config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require("jsonwebtoken")
+const cookiePerser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
 //middlewear
 app.use(express.json())
-app.use(cors())
-
-//
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yc8ov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+app.use(cors(
+  {
+    origin: ['http://localhost:5173','https://sapphire-hotel-bd5d7.web.app/','https://sapphire-hotel-bd5d7.firebaseapp.com/'],
+    credentials: true,
   }
-});
-const froomCollections = client.db('sapphireHotel').collection('featuredRooms');
-async function run() {
-  app.get('/featuredrooms',async(req,res)=>{
-  const cursor = froomCollections.find();
-  const result = await cursor.toArray();
-  res.send(result);
-  })
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+))
+
+app.use(cookiePerser())
+app.options('*', cors());
+
+
+const logger = (req, res, next) => {
+  console.log('Method & URL', req.method, req.url);
+  next();
 }
-run().catch(console.dir);
 
 
-app.get("/",(req,res)=>{
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    res.status(401).send({ message: 'unauthorized access' })
+
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if (err) {return res.status(401).send({message:'unauthorized'})
+      req.user =decoded;
+      next()
+     }     
+  })
+}
+
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yc8ov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  const froomCollections = client.db('sapphireHotel').collection('featuredRooms');
+  const bookedRooms = client.db('sapphireHotel').collection('bookings');
+  async function run() {
+    app.get('/featuredrooms', async (req, res) => {
+      const cursor = froomCollections.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    app.get('/featuredrooms/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await froomCollections.findOne(query);
+      res.send(result);
+    })
+    app.post('/bookings', async (req, res) => {
+      const newBookings = req.body;
+      const result = await bookedRooms.insertOne(newBookings);
+      res.send(result);
+    })
+    app.get('/bookings', async (req, res) => {
+      const cursor = bookedRooms.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    app.get('/bookings/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const cursor = bookedRooms.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    app.put('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedBookings = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const bookings = {
+        $set: {
+          date: updatedBookings.date,
+          contact: updatedBookings.contact
+        }
+      };
+
+      const result = await bookedRooms.updateOne(filter, bookings, options);
+      res.send(result);
+    })
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookedRooms.deleteOne(query);
+      res.send(result);
+    })
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      console.log(req.cookies);
+      const token=jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      console.log(token);
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+        maxAge: 3600000,
+      })
+        .send({ success: true })
+    })
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      // await client.connect();
+      // Send a ping to confirm a successful connection
+      // await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      // await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+  app.get("/", (req, res) => {
     res.send("Sapphire hotel server is running")
-})
+  })
 
-app.listen(port, ()=>{
-    console.log("Sapphire hotel server is running on ",port);
-})
+  app.listen(port, () => {
+    console.log("Sapphire hotel server is running on ", port);
+  })
